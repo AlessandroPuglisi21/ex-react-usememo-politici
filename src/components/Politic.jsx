f8bb6a11bcb95a68bc2b395349f1f3613a22cd77
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2025-03-18 18:36:41 +0100

    ADD:Filtro

--- src/components/Politic.jsx
+++ src/components/Politic.jsx
@@ -1,9 +1,9 @@
-
 import { useState, useEffect } from "react";
 
 function ListaPolitici() {
-
-    const [politico, setPolitico] = useState([])
+    const [politico, setPolitico] = useState([]);
+    const [filteredPolitico, setFilteredPolitico] = useState([]);
+    const [search, setSearch] = useState('');
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -16,35 +16,58 @@ function ListaPolitici() {
                 return response.json();
             })
             .then((data) => {
-                console.log(data)
-                setPolitico(data)
-                setLoading(false)
+                setPolitico(data);
+                setFilteredPolitico(data);
+                setLoading(false);
             })
             .catch((error) => {
-                setError(error)
-                setLoading(false)
-            })
-    }, [])
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    const handleChange = (event) => {
+        const value = event.target.value.toLowerCase();
+        setSearch(value);
+
+        const filtered = politico.filter((item) =>
+            item.name.toLowerCase().includes(value)
+            || item.position.toLowerCase().includes(value)
+            || item.biography.toLowerCase().includes(value)
+        );
+        setFilteredPolitico(filtered);
+    };
 
     if (loading) return <p>Caricamento...</p>;
-    if (error) return <p>Errore: {error}</p>;
+    if (error) return <p>Errore: {error.message}</p>;
 
     return (
-        <div className="max-w-7xl mx-auto p-4">
-            <h1 className="text-3xl font-bold text-center mb-6">Lista Politici</h1>
-            <div className="grid md:grid-cols-4 gap-6">
-                {politico.map((item, index) => (
-                    <div key={index} className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden">
-                        <img src={item.image} alt={item.name} className="w-full h-70 object-center" />
-                        <div className="p-4">
-                            <h2 className="text-xl font-semibold text-blue-600 dark:text-sky-400">{item.name}</h2>
-                            <h3 className="text-gray-700 dark:text-gray-300">{item.position}</h3>
-                            <p className="text-gray-600 dark:text-gray-400 mt-2">{item.biography}</p>
-                        </div>
-                    </div>
-                ))}
+        <>
+            <div>
+                <input
+                    type="text"
+                    className="bg-white text-black rounded-sm"
+                    placeholder="Cerca info..."
+                    value={search}
+                    onChange={handleChange}
+                />
             </div>
-        </div>
+            <div className="max-w-7xl mx-auto p-4">
+                <h1 className="text-3xl font-bold text-center mb-6">Lista Politici</h1>
+                <div className="grid md:grid-cols-4 gap-6">
+                    {filteredPolitico.map((item, index) => (
+                        <div key={index} className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden">
+                            <img src={item.image} alt={item.name} className="w-full h-70 object-center" />
+                            <div className="p-4">
+                                <h2 className="text-xl font-semibold text-blue-600 dark:text-sky-400">{item.name}</h2>
+                                <h3 className="text-gray-700 dark:text-gray-300">{item.position}</h3>
+                                <p className="text-gray-600 dark:text-gray-400 mt-2">{item.biography}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
 
